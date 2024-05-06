9bfee739b5f0ce04420b0aaf36e39013b15f3923
NickelannG <8836@holbertonstudents.com>
2024-05-07 09:59:41 +1000

    removing extra space

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 str = "abcdefghijklmnopqrstuvwxyz"
 new_str = ""  # empty string
-for i in range(1, len(str)):
+for i in range(1, len(str) + 1):
     new_str += str[i - 1]
 print("{}".format(new_str))
