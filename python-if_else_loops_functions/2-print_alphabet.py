62e24000abe301ff9beefc616494b9475c327dd8
NickelannG <8836@holbertonstudents.com>
2024-05-07 11:17:56 +1000

    removing variable

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "abcdefghijklmnopqrstuvwxyz"
-new_str = ""  # empty string
 for i in range(len(str)):
-    new_str += str[i]
-print("{}".format(new_str))
+    str[i]
+print("{}".format(str))
