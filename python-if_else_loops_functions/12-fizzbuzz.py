6841cd21aaf09a72c50158435acdb0b1771a6e82
NickelannG <8836@holbertonstudents.com>
2024-05-07 21:02:49 +1000

    task 12 fix: formatting

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 n = 100
-for i in range(1, n + 1):
+for i in range(1, n + 2):
     if (i % 3) == 0 and (i % 5) == 0:
         print("FizzBuzz ", end="")
     elif (i % 3) == 0:
         print("Fizz ", end="")
     elif (i % 5) == 0:
         print("Buzz ", end="")
+    elif i == 101:
+        print("$")
     else:
         print(i, " ", end="")
