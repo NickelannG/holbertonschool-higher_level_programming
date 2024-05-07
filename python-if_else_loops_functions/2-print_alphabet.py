2fea7cad247913feafc02f8b395e0aaaa6cd26db
NickelannG <8836@holbertonstudents.com>
2024-05-07 11:39:16 +1000

    removing extra space

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "abcdefghijklmnopqrstuvwxyz"
-str_strip = str.strip()
-for i in range(len(str_strip)):
-    str_strip[i]
-print("{}".format(str_strip))
+for i in range(len(str)-1):
+    str[i-1]
+print("{}".format(str))
