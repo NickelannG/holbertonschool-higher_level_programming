4f5b7f25c51f8813ac7a2a01f477160e62560330
NickelannG <8836@holbertonstudents.com>
2024-05-08 15:26:17 +1000

    define a = 1 and b = 2

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
+    a = 1
+    b = 2
+    added = add(1, 2)
     print("{a} + {b} = {added}".format(a=1, b=2, added=add(1, 2)))
