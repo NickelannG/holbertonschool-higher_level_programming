99a2da57ee62e9dbe258f3d004a19799a5cfab03
NickelannG <8836@holbertonstudents.com>
2024-05-08 16:30:18 +1000

    Task 1 fix: only use calculator_1 once

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,18 +2,12 @@
 if __name__ == "__main__":
     a = 10
     b = 5
-    from calculator_1 import add
     added = a + b
-    print("{} + {} = {}".format(a, b, added))
-
-    from calculator_1 import sub
     subbed = a - b
-    print("{} - {} = {}".format(a, b, subbed))
-
-    from calculator_1 import mul
     product = a * b
-    print("{} * {} = {}".format(a, b, product))
-
-    from calculator_1 import div
     divided = a / b
+    from calculator_1 import add, sub, mul, div
+    print("{} + {} = {}".format(a, b, added))
+    print("{} - {} = {}".format(a, b, subbed))
+    print("{} * {} = {}".format(a, b, product))
     print("{} / {} = {:.0f}".format(a, b, divided))
