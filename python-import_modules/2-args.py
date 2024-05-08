392f23c4acd4dda204a1018181f90132d44ff4cd
NickelannG <8836@holbertonstudents.com>
2024-05-08 18:20:07 +1000

    handled 0 arguments case

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
         print(f"{n-1} argument:")
         for i in range(1, n):
             print(f"{n-1}:", sys.argv[i])
+    elif n == 1:
+        print(f"{n-1} arguments.")
     else:
         print(f"{n-1} arguments:")
         for i in range(1, n):
