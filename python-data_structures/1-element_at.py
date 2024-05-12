74bbc5872dc18f8739dbe06874e617353447beab
NickelannG <8836@holbertonstudents.com>
2024-05-12 11:05:59 +1000

    Task 1 fix: check if idx is also equal to len

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,9 +2,7 @@
 
 def element_at(my_list, idx):
     for i in range(len(my_list)):
-        if idx < 0:
-            return None
-        elif idx > len(my_list):
+        if idx < 0 or idx >= len(my_list):
             return None
         else:
             return my_list[idx]
