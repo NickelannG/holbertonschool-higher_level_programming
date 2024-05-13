794248b63c5df7cff34975001507efca0f7af42d
NickelannG <8836@holbertonstudents.com>
2024-05-13 16:41:36 +1000

    Task 11 fix: changed return for empty list

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -16,7 +16,7 @@ The new list after the item has been removed
 
 def delete_at(my_list=[], idx=0):
     if len(my_list) == 0:
-        return None
+        return []
 
     if idx < 0 or idx >= len(my_list):
         return my_list
