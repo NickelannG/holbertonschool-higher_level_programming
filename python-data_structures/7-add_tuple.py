0c5b7b7437563a444c896c732a1e3ac1bbd17715
NickelannG <8836@holbertonstudents.com>
2024-05-13 13:55:58 +1000

    Task 7 pycodestyle fixes and added description

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
+"""
+Adds 2 tuples
+
+Function description:
+This function takes two tuples with integers and adds the first two elements.
+The first element should be the adition of the first element of each argument.
+The second element should be the addition of the second element
+of each argument.
+
+Parameters:
+2 tuples with integers:
+- tuple_a
+- tuple_b
+
+Returns:
+The added tuple with 2 integers
+"""
+
 
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2:
         tuple_a += (0,) * (2 - len(tuple_a))
     if len(tuple_b) < 2:
         tuple_b += (0,) * (2 - len(tuple_b))
-        
+
     new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return (new_tuple)
