b2ffc7284e09ddb5cfc57f7582b4a1d8ed77dadd
NickelannG <8836@holbertonstudents.com>
2024-05-13 12:47:43 +1000

    Task 6 fix: no casting integers to a string

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -8,9 +8,9 @@ def print_matrix_integer(matrix=[[]]):
         return result
     for i in range(len(matrix)):  # rows
         for j in range(len(matrix[i])):  # elements in each row
-            result += str(matrix[i][j])
+            result += "{:d}".format(matrix[i][j])
             if j < len(matrix[i]) - 1:
                 result += " "
         if i < len(matrix) - 1:
             result += "\n"
-    print(result)
+    print("{}".format(result))
