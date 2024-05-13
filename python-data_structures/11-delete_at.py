86b7b8dd4fe3b20445d86e1237c730d12e89daa6
NickelannG <8836@holbertonstudents.com>
2024-05-13 16:39:50 +1000

    Task 11: Delete at

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Function Description:
+This function deletes an item at a specific position in a list
+without using the pop() function
+
+Parameters:
+- my_list=[]: the list
+- idx=0: the index of the item to be removed
+
+Returns:
+The new list after the item has been removed
+"""
+
+
+def delete_at(my_list=[], idx=0):
+    if len(my_list) == 0:
+        return None
+
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    del my_list[idx]
+    return my_list
