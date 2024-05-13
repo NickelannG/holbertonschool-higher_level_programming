90cec0ddcc4ae54f768d0b72cdaeaca45caee14e
NickelannG <8836@holbertonstudents.com>
2024-05-13 12:25:21 +1000

    task 6: list of lists = matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" prints a matrix of integers """
+
+
+def print_matrix_integer(matrix=[[]]):
+    result = ""
+    if not matrix:
+        return result
+    for i in range(len(matrix)):  # rows
+        for j in range(len(matrix[i])):  # elements in each row
+            result += str(matrix[i][j])
+            if j < len(matrix[i]) - 1:
+                result += " "
+        if i < len(matrix) - 1:
+            result += "\n"
+    print(result)
