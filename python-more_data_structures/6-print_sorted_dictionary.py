8df656ce4ec292b6c2d91762eb16f43e15115fff
NickelannG <8836@holbertonstudents.com>
2024-05-14 14:53:06 +1000

    Task 6 fix: empty dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,7 +5,9 @@ Function Description:
 This function that prints a dictionary by ordered keys.
 """
 
+
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = (sorted(a_dictionary.keys()))
-    format = map(lambda key: f"{key}: {a_dictionary[key]}", sorted_keys)
-    print(*format, sep='\n')
+    if sorted_keys:
+        format = map(lambda key: f"{key}: {a_dictionary[key]}", sorted_keys)
+        print(*format, sep='\n')
