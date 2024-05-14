0d689247e6fea3ef794ebb6f7b33f2a3dedb47a2
NickelannG <8836@holbertonstudents.com>
2024-05-14 14:48:21 +1000

    Task 6: revert to old cold

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,8 +5,7 @@ Function Description:
 This function that prints a dictionary by ordered keys.
 """
 
-
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = (sorted(a_dictionary.keys()))
-    for key in sorted_keys:
-        print(key, ":", a_dictionary[key])
+    format = map(lambda key: f"{key}: {a_dictionary[key]}", sorted_keys)
+    print(*format, sep='\n')
