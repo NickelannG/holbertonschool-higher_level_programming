6d1d68e92aae1b7a90b8d90b37e8a9a0cc72337d
NickelannG <8836@holbertonstudents.com>
2024-05-14 14:08:03 +1000

    Task 4 fix: used the correct function

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -7,4 +7,4 @@ This function that returns a set of all elements present in only one set.
 
 
 def only_diff_elements(set_1, set_2):
-    return (set_1.union(set_2))
+    return (set_1.symmetric_difference(set_2))
