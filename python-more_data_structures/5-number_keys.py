0386fe3cabfca6962d02c6e72f7816680c7be414
NickelannG <8836@holbertonstudents.com>
2024-05-14 14:16:30 +1000

    Task 5: number of keys

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+Function Description:
+This function that returns the number of keys in a dictionary.
+"""
+
+
+def number_keys(a_dictionary):
+    return (len(a_dictionary))
