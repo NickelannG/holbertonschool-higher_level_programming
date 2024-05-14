48fc3943ab14bf07146d317663037d35334ed371
NickelannG <8836@holbertonstudents.com>
2024-05-14 13:53:34 +1000

    Task 3: present in both

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+Function Description:
+This function that returns a set of common elements in two sets.
+"""
+
+
+def common_elements(set_1, set_2):
+    return (set_1.intersection(set_2))
