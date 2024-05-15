94814e1dcb58c295bef16ee8974eef47cbd0423c
NickelannG <8836@holbertonstudents.com>
2024-05-15 18:58:54 +1000

    Task 2 fix: handled index error separately

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -17,12 +17,13 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             if isinstance(my_list[i], int):
-                try:
-                    print("{:d}".format(int(my_list[i])), end="")
-                    count += 1
-                except ValueError:
-                    break
-        except (IndexError, TypeError):
+                print("{:d}".format(int(my_list[i])), end="")
+                count += 1
+        except (TypeError, ValueError):
             break
     print()
+    try:
+        my_list[x]
+    except IndexError:  # Do this if index x is out of range
+        return count
     return count
