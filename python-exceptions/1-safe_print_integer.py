5d8e6b8cccdf81bd23381125849426e5f05e4a82
NickelannG <8836@holbertonstudents.com>
2024-05-15 15:55:31 +1000

    Task 1: safe printing of an integers list

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Function Description:
+This function prints an integer with "{:d}".format()
+
+Parameters:
+- value: can be any type (integer,string etc)
+
+Returns:
+True if value has been correctly printed, otherwise False
+"""
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(int(value)))
+        return True
+    except ValueError:
+        return False
