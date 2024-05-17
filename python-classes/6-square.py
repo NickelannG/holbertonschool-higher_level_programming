45a2447a96590186da9f84575dd0379c78592b6d
NickelannG <8836@holbertonstudents.com>
2024-05-17 19:41:11 +1000

    Task 6: coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -2,10 +2,10 @@
 """
 6-square.py:
     This module contains a class Square that defines a square.
-    
+
 Classes:
     Square: represents a square.
-    
+
 Methods:
     - size(getter): A method that retrieves the value of
     private attribute, size.
@@ -20,36 +20,40 @@ class Square:
     """
     A class that defines a square.
     """
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """
         Initialises Square with the given attributes.
-        
+
         Parameters:
-            size (int): A private isntance attribute that has a starting
+            - size (int): A private isntance attribute that has a starting
             value of 0 and represents the size of a square.
+            - position: (tuple): a private instance attribute that has a
+            starting value of (0, 0) and represents the position of
+            the square.
         """
         self.__size = size
-    
+        self.__position = position
+
     @property
     def size(self):
         """
         A getter method within the Square class that retrieves the
         private attribute, size.
-        
+
         Returns:
             The private attribute, size.
         """
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         """
         A setter method within the Square class that sets a value to
         the private attribute, size.
-        
+
         Parameters:
             value: the value to set size to.
-            
+
         Raises:
             TypeError: If value is not an integer.
             ValueError: If value is a negative integer.
@@ -59,17 +63,46 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
+    @property
+    def position(self):
+        """
+        A getter method that retrieves the value of position.
+
+        Returns:
+            the private attribute, position.
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        A setter method that sets the value of private attribute,
+        position.
+
+        Parameters:
+            value (tuple): the value to set position to.
+
+        Raises:
+            TyperError: If position is not a tuple of 2 positive
+            integers.
+        """
+        if (not isinstance(value, tuple) or len(value) != 2
+            or not isinstance(value[0], int) or
+            not isinstance(value[1], int) or value[0] < 0
+                or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
     def area(self):
         """
         A method within the Square class that calculates
         the area of a square.
-        
+
         Returns:
             int: the area of a square (size to the power of 2).
         """
         return self.__size ** 2
-    
+
     def my_print(self):
         """
         A method within the Square class that prints in stdout a square
@@ -77,5 +110,7 @@ class Square:
         """
         if self.__size == 0:
             print()
+        if self.__position[1] > 0:
+            print()
         for i in range(self.__size):
-            print("#"*self.__size)
+            print("_"*self.__position[0] + "#"*self.__size)
