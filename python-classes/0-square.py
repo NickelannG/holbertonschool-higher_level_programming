bbb41fa93b9bfe728a498de7ef8d63f030d9dc30
NickelannG <8836@holbertonstudents.com>
2024-05-17 11:55:31 +1000

    Task 0 Docstring: added class documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """
-An empty class square that defines a square
+0-sqaure.py:
+This module shows an empty class "Square" that defines a square
+
+Classes:
+- Square: A class that represents a sqaure
 """
 
 
 class Square:
+    """
+    A class used to represent a Sqaure.
+
+    This class currently deos not have any attributes or methods
+    """
     pass
