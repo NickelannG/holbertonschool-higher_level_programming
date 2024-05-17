99cce89ab1470734beae13f18521fec95c4c9c4a
NickelannG <8836@holbertonstudents.com>
2024-05-17 21:24:18 +1000

    removing empty line

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -122,6 +122,6 @@ class Square:
             print()
             return
         if self.__position[1] > 0:
-            print()
+            print(end="")
         for i in range(self.__size):
             print(" "*self.__position[0] + "#"*self.__size)
