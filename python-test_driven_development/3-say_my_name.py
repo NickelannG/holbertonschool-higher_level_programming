a33928740789c4e83ffb1245e14736b03b9f2867
NickelannG <8836@holbertonstudents.com>
2024-05-20 16:43:02 +1000

    task 2: added docstring documentation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+"""
+3-say_my_name:
+    This module contains the say_my_name function
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Prints my name is <first name> <last name>
+
+    Parameters:
+        - first_name (str): the first name
+        - last_name (str): the last name
+
+    Raises:
+        - TypeError: if first name is not a string
+        - TypeError: if last-name is not a string
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
