55b29aff8c1dd6c3970d3f02cbf375b7530a4592
NickelannG <8836@holbertonstudents.com>
2024-05-21 13:18:36 +1000

    Task 0: Simple rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+0-rectangle.py:
+    This module has an empty class Rectangle that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    A class used to define a rectangle.
+    
+    This class currently doesn't have any methods or attributes.
+    """
+    pass
