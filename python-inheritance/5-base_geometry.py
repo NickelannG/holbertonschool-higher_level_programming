fd03ee885f9eb49f7f1c938ec87797e32ab10da3
NickelannG <8836@holbertonstudents.com>
2024-05-22 17:37:38 +1000

    task 5: Geometry module

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+5-base_geometry:
+    This module contains an empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    This class currently has no methods or attributes
+    """
+    pass
