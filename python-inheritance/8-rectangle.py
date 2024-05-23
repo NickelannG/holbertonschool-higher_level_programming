7d7b4e7e57236b449aee40bd2bb6ce4f0efc7be1
NickelannG <8836@holbertonstudents.com>
2024-05-23 11:55:44 +1000

    Task 8 change integer validator call

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle(BaseGeometry):
     Subclass Rectangle that inherits from Base Class BaseGeometry
     """
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        BaseGeometry.integer_validator("width", width)
+        BaseGeometry.integer_validator("height", height)
         self.__width = width
         self.__height = height
