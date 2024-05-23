58b1b6b328d72c3727f3b0da6c1238da740b7be3
NickelannG <8836@holbertonstudents.com>
2024-05-23 12:04:48 +1000

    change string format for error

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -31,5 +31,7 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         BaseGeometry.integer_validator(self, "width", width)
         BaseGeometry.integer_validator(self, "height", height)
+
+        
         self.__width = width
         self.__height = height
