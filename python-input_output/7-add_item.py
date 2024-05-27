cdf477099756bca3468ec7291a4dee9c6bdf85a3
NickelannG <8836@holbertonstudents.com>
2024-05-27 14:13:07 +1000

    task 7: document module

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+7-add_item:
+    This module contains a script that adds all arguments to a python
+    list, and saves them to a JSON file
+"""
 import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
