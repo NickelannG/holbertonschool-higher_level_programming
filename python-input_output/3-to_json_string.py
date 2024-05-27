5860c9045b8ac194eb148a199c6d09df51c454d4
NickelannG <8836@holbertonstudents.com>
2024-05-27 11:57:26 +1000

    docs

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -8,7 +8,7 @@ import json
 
 def to_json_string(my_obj):
     """
-    This function returns the JSON representation of an object (string).
+    This function returns the JSON representation of an object.
 
     Parameters:
         - my_obj: The python object to be converted
