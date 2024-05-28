07b063787cf5f7effbf81562085418b8c12cd8f1
NickelannG <8836@holbertonstudents.com>
2024-05-28 12:55:24 +1000

    correct error string format

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -19,5 +19,9 @@ def save_to_json_file(my_obj, filename):
         json_string = json.dumps(my_obj)
         with open(filename, "w", encoding="utf-8") as f:
             f.write(json_string)
+
+    except TypeError as e:
+        print(f"[PermissionError] {e}")
+
     except PermissionError as e:
         print(f"[PermissionError] {e}")
