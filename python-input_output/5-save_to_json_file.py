81553e68c38b0b3a9659d6082fb4fd7f35a9710f
NickelannG <8836@holbertonstudents.com>
2024-05-28 13:03:11 +1000

    TyperError

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -20,8 +20,8 @@ def save_to_json_file(my_obj, filename):
         with open(filename, "w", encoding="utf-8") as f:
             f.write(json_string)
 
-    except TypeError as e:
-        print(f"[TypeError] {e}")
-
     except PermissionError as e:
         print(f"[PermissionError] {e}")
+
+    except TypeError as e:
+        print(f"[TypeError] {e}")
