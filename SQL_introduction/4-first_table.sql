ba57d9e9cb9645c2446068134fef7e41b5cc7b02
NickelannG <8836@holbertonstudents.com>
2024-06-17 14:16:22 +1000

    docs

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
--- Creates a table called first_table in current database in MySQL server
+-- Creates a table called first_table in current database in MySQL server if it doesnt exists
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
