c126c6a55a0c8a61fa8d54093b11f3e241c6dfa6
NickelannG <8836@holbertonstudents.com>
2024-06-19 13:57:51 +1000

    Task 15

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- lists all Comedy shows in the imported hbtn_0d_tvshows database
+SELECT DISTINCT title FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title ASC;
