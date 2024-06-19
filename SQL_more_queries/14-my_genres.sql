6e282bca73da9e54c7edca69b2d888321553d97b
NickelannG <8836@holbertonstudents.com>
2024-06-19 11:32:09 +1000

    Task 14 fix: only one select

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,8 @@
 -- lists all genres of the show Dexter in the imported hbtn_0d_tvshows database.
-SET @dexter_id := 
-(SELECT id FROM tv_shows WHERE title = 'Dexter');
-
-SELECT name FROM tv_genres
+SELECT DISTINCT name FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = @dexter_id
-GROUP BY name
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY name ASC;
