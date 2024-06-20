5a7ea5c03e2327f4bfe26ce30e8a657d01833612
NickelannG <8836@holbertonstudents.com>
2024-06-20 12:21:21 +1000

    Task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Takes in the name of a state as an argument and lists all cities of
+that states in the hbtn_0e_4_usa database.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cur = db.cursor()
+    state = sys.argv[4]
+    query = "SELECT cities.name FROM cities " + \
+        "LEFT JOIN states ON cities.state_id = states.id " + \
+            "WHERE BINARY states.name = %s " + \
+            "ORDER BY cities.id ASC;"
+
+    cur.execute(query, (state,))
+
+    for row in cur.fetchall():
+        print(row[0] + ", ", end="")
+    print()
