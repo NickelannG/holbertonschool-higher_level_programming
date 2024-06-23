d48827ec6536b73c131dec3d1f16733595811012
NickelannG <8836@holbertonstudents.com>
2024-06-23 18:44:46 +1000

    revert to old code

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -18,21 +18,18 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     state = sys.argv[4]
-    query = "SELECT GROUP_CONCAT(cities.name ORDER BY cities.id ASC SEPARATOR ', ') " + \
-            "FROM cities " + \
-            "INNER JOIN states ON cities.state_id = states.id " + \
-            "WHERE BINARY states.name = %s;"
+    query = "SELECT cities.id, cities.name, states.id FROM cities " + \
+        "INNER JOIN states ON cities.state_id = states.id " + \
+            "WHERE BINARY states.name = %s " + \
+            "ORDER BY cities.id ASC;"
 
     cur.execute(query, (state,))
 
-    #i = 0
-    #cities = cur.fetchall()
-    #for row in cities:
-    #    print("{}".format(row[1]), end="")
-    #    i += 1
-    #    if i != len(cities):
-    #        print(", ", end="")
-    #print("")
-
-    for row in cur.fetchall():
-        print(row[0])
+    i = 0
+    cities = cur.fetchall()
+    for row in cities:
+        print("{}".format(row[1]), end="")
+        i += 1
+        if i != len(cities):
+            print(", ", end="")
+    print("")
