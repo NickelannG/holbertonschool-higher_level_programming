bfacd7ec2b65ae5f5d3216687e790e3f9c5c049e
NickelannG <8836@holbertonstudents.com>
2024-06-23 18:43:12 +1000

    using group_concat

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -18,18 +18,21 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     state = sys.argv[4]
-    query = "SELECT cities.id, cities.name, states.id FROM cities " + \
-        "INNER JOIN states ON cities.state_id = states.id " + \
-            "WHERE BINARY states.name = %s " + \
-            "ORDER BY cities.id ASC;"
+    query = "SELECT GROUP_CONCAT(cities.name ORDER BY cities.id ASC SEPARATOR ', ') " + \
+            "FROM cities " + \
+            "INNER JOIN states ON cities.state_id = states.id " + \
+            "WHERE BINARY states.name = %s;"
 
     cur.execute(query, (state,))
 
-    i = 0
-    cities = cur.fetchall()
-    for row in cities:
-        print("{}".format(row[1]), end="")
-        i += 1
-        if i != len(cities):
-            print(", ", end="")
-    print("")
+    #i = 0
+    #cities = cur.fetchall()
+    #for row in cities:
+    #    print("{}".format(row[1]), end="")
+    #    i += 1
+    #    if i != len(cities):
+    #        print(", ", end="")
+    #print("")
+
+    for row in cur.fetchall():
+        print(row[0])
