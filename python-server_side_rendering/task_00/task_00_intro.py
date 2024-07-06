ad2f2551c7b5f2eea93e6c138e8d387674e42838
NickelannG <8836@holbertonstudents.com>
2024-07-06 19:28:31 +1000

    Task 0 fix - N/A value

--- python-server_side_rendering/task_00/task_00_intro.py
+++ python-server_side_rendering/task_00/task_00_intro.py
@@ -8,7 +8,11 @@ import sys
 def generate_invitations(template, attendees):
     """
     This function generates personlised invitation files from a
-    template with placeholders and a list of objects
+    template with placeholders and a list of objects.
+
+    parameters:
+    - Template (str): 
+    - attendees (list):
     """
     if not isinstance(template, str):
         logging.error("Invalid input type: template must be a string.")
@@ -21,27 +25,32 @@ def generate_invitations(template, attendees):
         sys.exit()
 
     if template == "":
-        logging.error("template must not be an empty string")
+        logging.error("Template is empty, no output files generated.")
         sys.exit()
 
     if attendees == []:
-        logging.error("attendees must not be an empty list")
+        logging.error("No data provided, no output files generated.")
         sys.exit()
 
     # Process each attendee
-    # if a value is missing, replace with default value "N/A"
     # count index of added attendees starting from 1
     i = 1
+    
+    # iterate through each dictionary in the attendees list
     for attendee in attendees:
-        processed_template = template.format(
-            name=attendee.get('name', 'N/A'),
-            event_title=attendee.get('event_title', 'N/A'),
-            event_date=attendee.get('event_date', 'N/A'),
-            event_location=attendee.get('event_location', 'N/A')
-        )
         i += 1
+        for key, value in attendee.items():
+            if value is not None:
+                # using the ** operator to unpack dictionary
+                processed_template = template.format(**attendee)
+            else:
+                # if any value in the dctionary is None - replace with N/A
+                attendee[key] = "N/A"
 
         output_file = "output_{}.txt".format(i)
         # Write to output file
-        with open(output_file, 'w') as f:
-            f.write(processed_template)
+        try: 
+            with open(output_file, 'w') as f:
+                    f.write(processed_template)
+        except IOError:
+            logging.error("Failed to write to file")
