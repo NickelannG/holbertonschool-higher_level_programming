e9687cb59a0ff8874632aeceb1675e701ca0e592
NickelannG <8836@holbertonstudents.com>
2024-07-06 19:42:58 +1000

    task 0 docs

--- python-server_side_rendering/task_00/task_00_intro.py
+++ python-server_side_rendering/task_00/task_00_intro.py
@@ -11,8 +11,10 @@ def generate_invitations(template, attendees):
     template with placeholders and a list of objects.
 
     parameters:
-    - Template (str): 
-    - attendees (list):
+    - template (str): string containing placeholders to be replaced
+    (template.txt).
+    - attendees (list): list of dictionaries where each dict represents an
+    attendee/recipient of invitation.
     """
     if not isinstance(template, str):
         logging.error("Invalid input type: template must be a string.")
