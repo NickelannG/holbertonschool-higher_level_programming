a9208d2f1bbec0e75bae14196c9542e3a84c0f4a
NickelannG <8836@holbertonstudents.com>
2024-07-08 12:42:36 +1000

    task 3: added routes from previous tasks

--- python-server_side_rendering/task_03/task_03_files.py
+++ python-server_side_rendering/task_03/task_03_files.py
@@ -8,6 +8,26 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    # Read and and load items from json file to python dict
+    with open('items.json') as f:
+        data = json.load(f)
+    # retrieve values of "items" key
+    # assign empty list if 'items' not present
+    items = data.get('items', [])
+
+    # Put items into html template
+    return render_template('items.html', items=items)
+
 @app.route('/products')
 def product():
     data = []
