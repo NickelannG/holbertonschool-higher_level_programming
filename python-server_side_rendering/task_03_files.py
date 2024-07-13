93a21bcb29fe4a0038fdadc3a3be00086846d954
NickelannG <8836@holbertonstudents.com>
2024-07-14 08:59:22 +1000

    Task 3: test for json and invalid id json

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
+"""Basic flask application that renders a HTML template"""
 from flask import Flask, render_template, request
-from pathlib import Path
 import json
 import csv
 
@@ -11,75 +10,77 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/items')
 def items():
-    items_list = []
+    # Read and and load items from json file to python dict
+    with open('items.json') as f:
+        data = json.load(f)
+    # retrieve values of "items" key
+    # assign empty list if 'items' not present
+    items = data.get('items', [])
 
-    with open("items.json", 'r') as f:
-        rows = json.load(f)
-    for key,value in rows.items():
-        items_list = value
-
-    return render_template('items.html', items=items_list)
+    # Put items into html template
+    return render_template('items.html', items=items)
 
 @app.route('/products')
-def products():
+def product():
+    data = []
     source = request.args.get('source')
     id = request.args.get('id')
 
-    data = []
+    if id is not None:
+        try:
+            id = int(id)
+        except ValueError:
+            return render_template('product_display.html', error="Invalid ID format")
+
     if source == "json":
         data = load_json_data("products.json", id)
     elif source == "csv":
         data = load_csv_data("products.csv", id)
+    else:
+        return render_template('product_display.html', error="Wrong source")
 
+    if not data:
+        return render_template('product_display.html', error="Product not found")
+    
     return render_template('product_display.html', data=data, source=source, id=id)
 
 def load_json_data(filename, wanted_id=None):
+    """ Load JSON data from file and returns as dictionary """
     data = []
-
-    if not Path(filename).is_file():
-        raise FileNotFoundError("Data file '{}' missing".format(filename))
-
     try:
         with open(filename, 'r') as f:
             rows = json.load(f)
-
         for row in rows:
-            key = str(row['id'])
-            if (wanted_id is not None and key == wanted_id):
-                product = {}
-                for k, v in row.items():
-                    product[k] = v
-                data.append(product)
-
-        if wanted_id and not data:
-            raise ValueError("Product with id '{}' not found in file '{}'".format(wanted_id, filename))
-
-    except ValueError as exc:
-        raise ValueError("Unable to load data from file '{}'".format(filename)) from exc
+            if wanted_id is None or row['id'] == wanted_id:
+                data.append(row)
+    except ValueError:
+        return []
 
     return data
 
-def load_csv_data(filename, wanted_id = None):
-    """ Load JSON data from file and returns as dictionary """
-
+def load_csv_data(filename, wanted_id=None):
+    """ Load CSV data from file and returns as dictionary """
     data = []
-
-    if not Path(filename).is_file():
-        raise FileNotFoundError("Data file '{}' missing".format(filename))
-
     try:
         with open(filename, 'r') as csvfile:
-            # using DictReader method to convert each row to a dictionary
             for row in csv.DictReader(csvfile):
-                if (wanted_id is not None and row['id'] == wanted_id) or (wanted_id is None):
+                row['id'] = int(row['id'])  # Convert ID to integer
+                if wanted_id is None or row['id'] == wanted_id:
                     data.append(row)
-    except ValueError as exc:
-        raise ValueError("Unable to load data from file '{}'".format(filename)) from exc
+    except ValueError:
+        return []
 
     return data
 
-# Set debug=True for the server to auto-reload when there are changes
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(debug=True, port=5000)
