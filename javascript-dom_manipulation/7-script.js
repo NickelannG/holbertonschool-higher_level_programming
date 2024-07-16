b52fc1421af60b73ab96b8adc4401e7db3ee7975
NickelannG <8836@holbertonstudents.com>
2024-07-16 22:58:57 +1000

    task 7: Star Wars movies

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,14 @@
+const response = fetch('https://swapi-api.hbtn.io/api/films/?format=json');
+response
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+
+    const moviesList = document.getElementById("list_movies");
+    
+    for (const movie of data.results) {
+      const newMovie = document.createElement("li");
+      newMovie.textContent = movie.title;
+      moviesList.appendChild(newMovie);
+    }
+  });
